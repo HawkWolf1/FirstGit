7406c34579976acf1e54d5fc469df970ddaa5f18
HawkWolf1 <singhavinash9fg@gmail.com>
2022-11-21 19:53:07 +0530

    done 1

---
+++ dom.js
@@ -0,0 +1,20 @@
+/*    console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+console.log(document.all[10]);
+console.log(document.forms);
+console.log(document.links);
+console.log(document.images);
+
+console.log(document.getElementById('header-title'))      */
+var headerTitle = document.getElementById('header-title')
+
+headerTitle.style.borderTop = 'solid 3px #000'
+headerTitle.style.borderRight = 'solid 3px #000'
+headerTitle.style.borderLeft = 'solid 3px #000'
+headerTitle.style.borderBottom = 'solid 3px #000'
+
