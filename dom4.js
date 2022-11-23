ad67bf1e005ae9aca50797dc0f32eeba89743072
HawkWolf1 <singhavinash9fg@gmail.com>
2022-11-23 18:58:30 +0530

    23 nov 6 58

---
+++ dom4.js
@@ -0,0 +1,21 @@
+var filter = document.getElementById('filter');
+filter.addEventListener('keyup', filterItems);
+
+function filterItems(e){
+    var text=e.target.value.toLowerCase();
+    console.log(text);
+}
+
+function filterItems(e){
+    var text = e.target.value.toLowerCase();
+
+    var items = itemList.getElementsbyTagName('Li')
+    Array.from(items).forEach(function(item)) {
+        var itemName= item.firstChild.textcontent;
+        if (itemName.toLowerCase().indexOf(text) != -1 ){
+            item.style.display = 'block'
+        }else{
+            item.style.display = 'none'
+        }
+    }
+}
