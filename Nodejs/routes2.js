e0b474b66b11e6155b124ce257c2b88f0f18c782
HawkWolf1 <singhavinash9fg@gmail.com>
2023-01-08 12:46:29 +0530

    8 jan 12 46

--- Nodejs/routes2.js
+++ Nodejs/routes2.js
@@ -1,17 +1,34 @@
 const fs = require('fs');
 
+
+
+
 const requestHandler = (req, res) => {
   const url = req.url;
   const method = req.method;
+
   if (url === '/') {
+    const filePath = path.join(__dirname, "message.txt")
+
+    fs.readFile(filePath, {encoding: "utf-8"}, (err,data) => {
+      if (err){
+        console.log(err)
+      }
+    console.log(`data from file` + data)  
     res.write('<html>');
     res.write('<head><title>Enter Message</title><head>');
+    res.write(`<body>${data}</body>`)
     res.write(
       '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write('</html>');
     return res.end();
+    })
+
   }
+
+
+
   if (url === '/message' && method === 'POST') {
     const body = [];
     req.on('data', chunk => {
@@ -20,14 +37,19 @@ const requestHandler = (req, res) => {
     });
     return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
+      console.log('parsedBody>>>>', parsedBody)
       const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, err => {
+      fs.writeFile('message.txt', message, (err) => {
+        if (err){
+          console.log(err)
+        }
+        console.log(`indise fs.writeFile`)
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
       });
     });
-    
+
   }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
