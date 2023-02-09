6b02392b7d70b67ac295eb4df6d0a4975026aa6f
HawkWolf1 <singhavinash9fg@gmail.com>
2023-02-09 11:43:23 +0530

    9 feb 11 43

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const express = require('express') // importing the express package
 
 const app = express() // express package can be used as a function
 
-app.use((req,res,next) =>{ // use allows to add a new middleware function // next is a function that is passed by this function in express js
-    console.log('learning Middleware') // next allows the request to travel on next middleware
+app.use((req, res, next) =>{ // use allows to add a new middleware function // next is a function that is passed by this function in express js
+    console.log('A') // next allows the request to travel on next middleware
+    next()
 })
 
-const server = http.createServer(app) // now this app (which is express function) can be passed thrugh the server
+app.use((req, res, next) =>{  
+    console.log('B') 
+    res.send('<h1>CC</h1>')
+})
 
-server.listen(4050)
+
+app.listen(4000) // we are creating server here directly
