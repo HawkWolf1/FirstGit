277300e5f2a1acbf709ce8ae2c63ad7f1d114cd7
HawkWolf1 <singhavinash9fg@gmail.com>
2023-02-10 11:55:15 +0530

    10 feb 11 55

--- app.js
+++ app.js
@@ -11,13 +11,15 @@ const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false})) //It does the whole body parsing ehich would have to be manually otherwise
 
-app.use(adminRoutes) // we dont need to call it like a function
+//sometimes the outsourced path have a common starting path e.g. admin in this case.
+//we do this to get a common path to direct the request to a common channel
+app.use('/admin', adminRoutes) // we dont need to call it like a function
 //order matters. we cant place it after the middleware present at the bottom
 
 app.use(shopRoutes)
 
 app.use((req,res,next) => { // we have a catch a;ll middleware at the bottom where we dont need a path filter but we could use slash if want
-    res.status(404).send('<h1> Page not foundddddddd</h1>') // when we write use it handles all http method not just post and get requests.
+    res.status(404).send('<h1> Page not foundddddddd!!!!</h1>') // when we write use it handles all http method not just post and get requests.
 })
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,13 @@ const express = require('express')
 
 const router = express.Router() // router is like a mini express app tied to another express which can be exported
 
-router.get('/admin/mycart', (req, res, next) =>{   
-    res.send('<form action="/mycart" method="POST"><input type ="text" name="title"><button type="submit">Add product</button></form>')
+//same path ca nbe used if method are different
+// either we can write it as /admin/mycart or we can do it as done in app.js file
+router.get('/mycart', (req, res, next) =>{   
+    res.send('<form action="/admin/mycart" method="POST"><input type ="text" name="title"><button type="submit">Add product</button></form>')
 })
 
-router.post('/admin/mycart', (req, res, next) =>{   //post tag is used to filter for post requests
+router.post('/mycart', (req, res, next) =>{   //post tag is used to filter for post requests
     console.log(req.body) //line 8 reaches here 
     res.redirect('/') 
 })
